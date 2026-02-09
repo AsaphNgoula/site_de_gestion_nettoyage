83698bfaefcdd083677dae01acef2bcb4cd5528b
asaph <asaphngoula237@gmail.com>
2026-02-09 11:01:25 +0100

    fix  added the database so that it should read from the env file

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -79,15 +79,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'pproclean',
-        'USER':'pproclean',
-        'PASSWORD':'PProclean2025quebec',
-        'HOST':'localhost',
-        'PORT':'5432',
-
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
 
